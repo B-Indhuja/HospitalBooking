709625a2f88b8d0e025ee03d5198b183a9cc1d36
Indhuja B <indhuja.B@fulcrumdigital.com>
2026-09-23 22:45:47 +0530

    Created doctor availability entity and shift enum

---
+++ src/main/java/com/project/HospitalBooking/entity/DoctorAvailability.java
@@ -0,0 +1,64 @@
+package com.project.HospitalBooking.entity;
+import com.project.HospitalBooking.enums.Shift;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="doctor_availability",uniqueConstraints = {@UniqueConstraint(columnNames = {"doctor_id", "availability_date", "shift"})})
+public class DoctorAvailability {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer availabilityId;
+
+    @ManyToOne
+    @JoinColumn(name="doctor_id",nullable = false)
+    private Doctor doctor;
+
+    @Column(nullable = false)
+    private LocalDate availabilityDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Shift shift;
+
+    @Column(nullable = false)
+    private Integer maxAppointments;
+
+    public Integer getAvailabilityId(){
+        return availabilityId;
+    }
+
+    public void setDoctor(Doctor doctor){
+        this.doctor=doctor;
+    }
+
+    public Doctor getDoctor(){
+        return doctor;
+    }
+
+    public void setAvailabilityDate(LocalDate date){
+        this.availabilityDate=date;
+    }
+
+    public LocalDate getAvailabilityDate(){
+        return availabilityDate;
+    }
+
+    public void setShift(Shift shift){
+        this.shift=shift;
+    }
+
+    public Shift getShift(){
+        return shift;
+    }
+
+    public void setMaxAppointments(Integer maxAppointments){
+        this.maxAppointments=maxAppointments;
+    }
+
+    public Integer getMaxAppointments(){
+        return maxAppointments;
+    }
+}
